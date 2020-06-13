173aed04118daa8237b93df4ca0548bf752c2bcd
sleeve <1421147715@qq.com>
2020-06-13 17:38:48 +0800

    ch01-simpleServlet
    测试容器完成

---
+++ Tomcat/ch02-SimpleServlet/src/main/java/org/sleeve/Request.java
@@ -0,0 +1,9 @@
+package org.sleeve;
+
+/**
+ * @author Sleeve
+ * @version 1.0
+ * @date 2020/6/13 17:37
+ */
+public class Request {
+}
---
+++ Tomcat/ch02-SimpleServlet/src/main/java/org/sleeve/verifyServlet.java
@@ -0,0 +1,41 @@
+package org.sleeve;
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 测试servlet应用程序
+ * @author Sleeve
+ * @version 1.0
+ * @date 2020/6/13 16:59
+ */
+public class verifyServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.print("Servlet初始化");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.print("处理服务");
+        PrintWriter out = servletResponse.getWriter();
+        out.println("hello");
+        out.println("123");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.print("duang,销毁  ");
+    }
+}
